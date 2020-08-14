41a70ed562992d7e21f5914ff101599e7cdd48a9
takazawa-shota <takazawa.shota@selrea.jp>
2020-08-14 21:33:15 +0900

    コミットだお！！

--- sample.php
+++ sample.php
@@ -3,34 +3,36 @@
 @section('content')
     <div class="container mt-2">
         <div class="border p-4">
-        
+
+            <div class="mb-4 text-right">
+                <a class="btn btn-primary" href="{{ route('posts.edit',['post' => $post]) }}">
+                    編集する
+                </a>
+                <form
+                    style="display:inline-block;"
+                    method="post"
+                    action="{{ route('posts.destroy',['post' => $post]) }}"
+                >
+                    @csrf
+                    @method('DELETE')
+                    <button class="btn btn-danger">削除する</button>
+                </form>
+            </div>
+
             <h1 class="h5 mb-4">
-                投稿の編集
+                {{ $post->title }}
             </h1>
+            
+            <p class="mb-4">
+                {{ nl2br($post->body) }}
+            </p>
 
-            <form class="mb-4" method="post" action="{{ route('posts.update',['post' => $post]) }}">
-                @csrf
-                @method('PUT')
-
-                <fieldset class="mb-4">
-                    <div class="form-group">
-                        <label for="title">
-                            タイトル
-                        </label>
-                        <input
-                            id="title"
-                            name="title"
-                            class="form-control {{ $errors->has('title') ? 'is-invalid' : '' }}"
-                            value="{{ old('title') ?: $post->title }}"
-                            type="text"
-                        >
-                        @if($errors->has('title'))
-                            <div class="invalid-feedback">
-                                {{ $errors->first('title') }}
-                            </div>
-                        @endif
-                    </div>
-
+            <section>
+                <h2 class="h5 mb-4">
+                    コメント
+                </h2>
+                
+                <form class="mb-4" method="post" action="{{ route('comments.store') }}">
                     <div class="form-group">
                         <label for="body">
                             本文
@@ -41,7 +43,7 @@
                             class="form-control {{ $errors->has('body') ? 'is-invalid' : '' }}"
                             rows="4"
                         >
-                            {{ old('body') ?: $post->body }}
+                            {{ old('body') }}
                         </textarea>
                         @if($errors->has('body'))
                             <div class="invalid-feedback">
@@ -51,15 +53,25 @@
                     </div>
 
                     <div class="mb-4">
-                        <a class="btn btn-secondary" href="{{ route('posts.show',['post' => $post]) }}">
-                            キャンセル
-                        </a>
                         <button class="btn btn-primary">
-                            編集する
+                            コメントする
                         </button>
+                    <div>
+                </form>
+
+                @forelse($post->comments as $comment)
+                    <div class="border-top mb-4">
+                        <time class="text-secondary">
+                            {{ $comment->created_at->format('Y.m.d H:i') }}
+                        </time>
+                        <p class="mt-2">
+                            {{ nl2br($comment->body) }}
+                        </p>
                     </div>
-                </fieldset>
-            </form>
+                @empty
+                    <p>コメントはまだありません。</p>
+                @endforelse
+            </section>
 
         </div>
     </div>
